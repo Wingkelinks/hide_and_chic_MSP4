19f3be383cfb52110e587b56df51fc3649f2e92c
Whitney Turner <whitters.t@gmail.com>
2021-09-29 22:45:15 +1000

    Add Sale to Categories & Add Category Filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.contrib import messages
 from django.db.models import Q, query_utils
-from .models import Product
+from .models import Category, Product
 # Use object Q from Django to handle filtering search by name and or description
 
 def all_products(request):
@@ -9,20 +9,27 @@ def all_products(request):
     
     products = Product.objects.all()
     query = None
+    category = None
     
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Make sure you've entered a search criteria!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
     
     return render(request, 'products/products.html', context)
