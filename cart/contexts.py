0f01db985fa45ba3384c7a73c2a7e8ce79c461f7
Whitney Turner <whitters.t@gmail.com>
2021-10-03 17:08:15 +1000

    Update Cart to Reflect Size Quantities

--- cart/contexts.py
+++ cart/contexts.py
@@ -22,12 +22,13 @@ def cart_contents(request):
                 'product': product,
             })
         else:
+            product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 cart_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
