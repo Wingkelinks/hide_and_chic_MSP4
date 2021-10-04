5b1011342f83ad7210be9460eecb3db28a462385
Whitney Turner <whitters.t@gmail.com>
2021-10-04 14:46:03 +1000

    Fix Refresh Cart Action

--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -1,8 +1,12 @@
+# __init__.py file ensures that cart_tools.py is treated as Python package
+# This allows cart_tools module to be available for imports and use in templates
+
 from django import template
 
 
 register = template.Library()
 
+# decorator to register function as template filter
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
