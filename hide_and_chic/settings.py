4056526be94c8df9c8a32503cafa0dc7bd86809d
Whitney Turner <whitters.t@gmail.com>
2021-10-09 20:06:06 +1000

    Add Checkout Views & Templates

--- hide_and_chic/settings.py
+++ hide_and_chic/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +67,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hide_and_chic.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
