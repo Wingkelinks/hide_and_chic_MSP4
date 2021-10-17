80fd8a7168d915274aa160da692365836ac1d81d
Whitney Turner <whitters.t@gmail.com>
2021-10-17 15:22:52 +1000

    Add Class Methods to WH Handler & WH View

--- hide_and_chic/settings.py
+++ hide_and_chic/settings.py
@@ -210,6 +210,8 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'aud'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+
 
 
 # Default primary key field type
